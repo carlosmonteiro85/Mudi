56aec231abc2f3aa08129d77ac52def1c5c0df85
Carlos Monteiro <carlosmonteiro1985@outlook.com>
2021-03-08 17:10:52 -0300

    Auatenticação com usuario do banco via JDBC

--- mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,19 +1,21 @@
 package br.com.alura.mvc.mudi;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,24 +25,46 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.formLogin(form -> form
 					.loginPage("/login")// URL da pagina de login
+					.defaultSuccessUrl("/home", true) //pagina usada ao autenticar
 					.permitAll() // todos são permitidos acessar a pagina de login
 				)
 			 		.logout(logout -> logout.logoutUrl("/logout")); 
 		
 	}
 	
-	@Bean
 	@Override
-	//Criando um usuario em memoria
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder() //Meodo apenas para fins de testes, não seguro para produção
-				.username("asdf") // user name do usuario
-				.password("asdf") // password
-				.roles("ADM")     // tipo
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); //gerador de senhas criptografadas
+		
+////		apenas para criar um usuario no banco
+//		UserDetails user =
+//				 User.builder() //Contruindo um usuario com a aplicação
+//					.username("asdf2") // user name do usuario
+//					.password(encoder.encode("asdf")) // criando o password e criptografando
+//					.roles("ADM")     // tipo
+//					.build();
+//		
+		
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)//instanciando a conexão
+		.passwordEncoder(encoder); //criptografar a senha
+		
+//		.withUser(user); // apenas se estiver criando um usuario a cima
 	}
+	
+	
+//	@Bean
+//	@Override
+//	//Criando um usuario em memoria
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder() //Meodo apenas para fins de testes, não seguro para produção
+//				.username("asdf") // user name do usuario
+//				.password("asdf") // password
+//				.roles("ADM")     // tipo
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
 
 }
